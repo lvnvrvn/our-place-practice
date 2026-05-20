989daecec0f3c84b07f671d32ac2ac176895e7da
lvnvrvn <lvnvrvn@yandex.ru>
2026-05-20 16:39:06 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const mobileMenu = document.querySelector(".mobile__menu");
-const mobileMenuIcon = document.querySelectorAll(".mobile__menu__icon");
-const mobileMenuList = document.querySelector(".mobile__menu__list");
-
-mobileMenu.addEventListener("click", function () {
-  mobileMenuIcon[0].classList.toggle("active");
-  mobileMenuIcon[1].classList.toggle("active");
-  mobileMenuList.classList.toggle("active");
-});
